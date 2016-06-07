8a95de95927d217b9fcf770bf8d168708b8d11d5
kdent <kdent@parc.com>
2016-06-06 17:38:49 -0700

    Add test cases for the DialogContext object.

---
+++ src/test/java/com/seaglass/alexa/DialogContextTest.java
@@ -0,0 +1,22 @@
+package com.seaglass.alexa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DialogContextTest {
+
+    @Test
+    public void testSetStateAsString() {
+        DialogContext dialogContext = new DialogContext();
+        dialogContext.setCurrentState("IN_LIST");
+        assertEquals(DialogManager.State.IN_LIST, dialogContext.getCurrentState());
+    }
+
+    @Test
+    public void testSetInvalidState() {
+        DialogContext dialogContext = new DialogContext();
+        dialogContext.setCurrentState("INVALID_STATE");
+        assertEquals(DialogManager.State.END, dialogContext.getCurrentState());
+    }
+}
